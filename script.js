5cea08da94dc9c8a1bb9fadee17af56617b3b05b
Mahesh Kumar K <maheshkanattil@gmail.com>
2022-12-01 02:40:12 +0530

    Final file

---
+++ script.js
@@ -0,0 +1,11 @@
+function clicked(val){
+    document.getElementById("screen").value+=val
+}
+function cleared(){
+    document.getElementById("screen").value=""
+}
+function equalclick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
